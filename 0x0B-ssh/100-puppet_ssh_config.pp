dd88fc17c15ca979cda4290b8d1064d8b3117267
Treasure-B <thabiso.magagula@gmail.com>
2024-04-27 17:05:03 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# using Puppet to make changes to our configuration file
+
+file { '/etc/ssh/ssh_config':
+ensure => present,
+}
+
+file_line { 'Turn off password auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => 'PasswordAuthentication no',
+  match              => '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path               => '/etc/ssh/ssh_config',
+  line               => 'IdentityFile ~/.ssh/school',
+  match              => '^#IdentityFile',
+}
